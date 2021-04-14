963c159d2e0f88c77b89a2b05410fa67af1051c1
demamama <82538255+demamama@users.noreply.github.com>
2021-04-14 21:47:25 +0300

    Add files via upload

---
+++ rubbish.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rubbish : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "bucket")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ spawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _template;
+    [SerializeField] private Transform _spawnpoint;
+    void Update()
+    {
+        Instantiate(_template, _spawnpoint.position, Quaternion.identity);
+    }
+}
